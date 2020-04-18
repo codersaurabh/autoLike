638e00e6688c1481f089a358b2df1b0dcb5bf1ea
Saurabh Kumar <devilsaurabh99@gmail.com>
2020-04-18 11:16:44 +0530

    OKC Like script

---
+++ autolike_okc.js
@@ -0,0 +1,32 @@
+// copy paste script as it is into your developer console.
+
+setInterval(function () {
+  var desktopLikeButton = document.getElementsByClassName(
+    ".doubletake-like-button"
+  )[0];
+  var mobileLikeButton = document.getElementsByClassName(
+    "quickmatch-button-like"
+  )[0];
+
+  (desktopLikeButton || mobileLikeButton).click();
+}, 600);
+
+
+// -- use this script if you want to like online user only --
+
+setInterval(function () {
+  var desktopLikeButton = document.getElementsByClassName(
+    ".doubletake-like-button"
+  )[0];
+  var desktopUnlikeButton = document.getElementsByClassName(
+    "doubletake-pass-button"
+  )[0];
+  let userStatus = document.getElementsByClassName(
+    "cardstatuses-status online"
+  );
+  if (typeof userStatus !== "undefined" && userStatus.length > 0) {
+    desktopLikeButton.click();
+  } else {
+    desktopUnlikeButton.click();
+  }
+}, 600);
